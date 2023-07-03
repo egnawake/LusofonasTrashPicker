9c1adadc841c67f1762154d46f7b8235086f11c4
João Inácio <egnawake@gmail.com>
2023-07-03 19:00:38 +0100

    Lay out the algorithm steps

--- Assets/Scripts/GeneticAlgorithm/GeneticAlgorithm.cs
+++ Assets/Scripts/GeneticAlgorithm/GeneticAlgorithm.cs
@@ -4,12 +4,31 @@ namespace GA
 {
     public class GeneticAlgorithm<T>
     {
+        private int maxGenerations;
+        private int populationSize;
         private Individual<T>[] population;
         private Func<Individual<T>> generator;
+        private Func<Individual<T>, float> evaluator;
+        private Func<Individual<T>[], Individual<T>[]> breeder;
+        private Func<Individual<T>[], Individual<T>[]> mutator;
+        private float bestFitness;
+        private Individual<T> bestIndividual;
 
-        public GeneticAlgorithm(int populationSize, Func<Individual<T>> generator)
+        public GeneticAlgorithm(int populationSize, int maxGenerations,
+            Func<Individual<T>> generator,
+            Func<Individual<T>, float> evaluator,
+            Func<Individual<T>[], Individual<T>[]> breeder,
+            Func<Individual<T>[], Individual<T>[]> mutator)
         {
+            this.populationSize = populationSize;
+            this.maxGenerations = maxGenerations;
+            this.generator = generator;
+            this.evaluator = evaluator;
+            this.breeder = breeder;
+            this.mutator = mutator;
+
             population = new Individual<T>[populationSize];
+            bestFitness = float.MinValue;
 
             for (int i = 0; i < populationSize; i++)
             {
@@ -19,7 +38,29 @@ namespace GA
 
         public Individual<T> Run()
         {
-            return null;
+            for (int g = 0; g < maxGenerations; g++)
+            {
+                // Evaluate fitness
+                for (int i = 0; i < populationSize; i++)
+                {
+                    population[i].Fitness = evaluator.Invoke(population[i]);
+
+                    if (population[i].Fitness.HasValue
+                        && population[i].Fitness.Value > bestFitness)
+                    {
+                        bestIndividual = population[i];
+                        bestFitness = population[i].Fitness.Value;
+                    }
+                }
+
+                // Mating
+                Individual<T>[] newPopulation = breeder.Invoke(population);
+
+                // Mutation
+                newPopulation = mutator.Invoke(newPopulation);
+            }
+
+            return bestIndividual;
         }
     }
 }
