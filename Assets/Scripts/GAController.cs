f233bdfdab0d79a66caca61923bd8f9003345c54
João Inácio <egnawake@gmail.com>
2023-07-07 13:54:11 +0100

    Make GA configurable in Unity editor

--- Assets/Scripts/GAController.cs
+++ Assets/Scripts/GAController.cs
@@ -4,17 +4,34 @@ using GA;
 
 public class GAController : MonoBehaviour
 {
+    [SerializeField] private int populationSize = 200;
+    [SerializeField] private int generations = 10;
+    [SerializeField] private float mutationChance = 0.1f;
+    [SerializeField] private int gameRuns = 100;
+    [SerializeField] private int gridRows = 10;
+    [SerializeField] private int gridColumns = 10;
+    [SerializeField] private int maxTurns = 200;
+    [SerializeField] private float trashProbability = 0.2f;
+
+    private int geneNumber = 243;
+
     private void Start()
     {
         Random rng = new Random();
 
+        TrashPickerRunner runner = new TrashPickerRunner(gameRuns, gridRows,
+            gridColumns, maxTurns, trashProbability);
+
         GeneticAlgorithm<RobotAction> ga = new GeneticAlgorithm<RobotAction>(
-            100, 10, new RandomEnumGenerator<RobotAction>(243).Generate,
-            new TrashPickerRunner().Evaluate,
+            populationSize, generations,
+            new RandomEnumGenerator<RobotAction>(geneNumber).Generate,
+            runner.Evaluate,
             new SplitBreeder<RobotAction>().Breed,
-            new RandomMutator<RobotAction>(0.1).Mutate);
+            new RandomMutator<RobotAction>(mutationChance).Mutate);
 
         Individual<RobotAction> result = ga.Run();
+
+        Debug.Log(result);
         Debug.Log(result.Fitness);
     }
 }
